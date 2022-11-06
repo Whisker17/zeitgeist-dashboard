9b9a23cd27f7d31832c9242175a92d16cabecd7e
Whisker17 <yukaijun123456@163.com>
2022-11-06 22:54:09 +0800

    update

--- src/components/layout/SideBar/NavItem.tsx
+++ src/components/layout/SideBar/NavItem.tsx
@@ -1,29 +1,75 @@
 import React from "react";
+import Box from "@chakra-ui/layout"
 import {
   Flex,
   Text,
   Link,
   Menu,
+  MenuItem,
   MenuButton,
   Icon,
 } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { IconType } from "react-icons/lib";
 
-interface iNavItem {
-  icon: IconType;
-  title: string;
-  description?: string;
-  active?: boolean;
+export interface iNavItem {
+  children: any;
+  href: string;
+  icon?: any;
+  onSelect?: () => void;
+}
+
+interface NavItemsProps {
+  menus: iNavItem[];
+  text?: string;
+  mainGroupTitle?: string;
+  icon?: any;
   navSize: string;
 }
 
-export default function NavItem({
-  icon,
-  title,
-  active,
-  navSize,
-}: iNavItem) {
+export default function NavItem({ menus, text, mainGroupTitle, icon, navSize }: NavItemsProps) {
+  const renderMenus = (menusToRender: iNavItem[]) => {
+    return menusToRender.map((menu, index: number) => {
+      const { children, icon: menuItemIcon, href, onSelect } = menu;
+      return typeof children === "string" ? (
+        <MenuItem
+          as={Link}
+          isExternal
+          href={href}
+          key={`link-${href}`}
+          fontSize="14px"
+          iconSpacing={4}
+          icon={
+            menuItemIcon && (
+              <Box w="20px">
+                <FontAwesomeIcon fontSize="16px" icon={menuItemIcon} />
+              </Box>
+            )
+          }
+        >
+          {children}
+        </MenuItem>
+      ) : (
+        // eslint-disable-next-line react/no-array-index-key
+        <MenuItem
+          onClick={() => {
+            if (onSelect) {
+              onSelect();
+            }
+          }}
+          cursor="pointer"
+          fontSize="14px"
+          // eslint-disable-next-line react/no-array-index-key
+          key={`link-${href}-${index}`}
+          href={href}
+          as={Flex}
+        >
+          {children}
+        </MenuItem>
+      );
+    });
+  };
   return (
     <Flex
       mt={30}
